0be130ccdaf9e1e8c07bd328e5606e66e9692438
Jørgen Grythe <jorgen.grythe@gmail.com>
2015-10-27 14:49:22 +0100

    Added help text and changed some variable names

--- plot/plotBeampattern.m
+++ plot/plotBeampattern.m
@@ -1,11 +1,30 @@
-function [] = plotBeampattern(xPos, yPos, w, f, c, dBmin, thetaScanningAngle, thetaSteeringAngle)
+function [] = plotBeampattern(xPos, yPos, w, f, c, dBmin, thetaScanningAngles, thetaSteeringAngle)
+%plotBeampattern - plots the beampattern for various frequencies
+%
+%plotBeampattern(xPos, yPos, w, f, c, dBmin, thetaScanningAngles, thetaSteeringAngle)
+%
+%IN
+%xPos                - 1xP vector of x-positions [m]
+%yPos                - 1xP vector of y-positions [m]
+%w                   - 1xP vector of element weights
+%f                   - Wave frequency [Hz]
+%c                   - Speed of sound [m/s]
+%thetaScanningAngles - 1xN vector of theta scanning angles [degrees]
+%thetaSteeringAngle  - 1x1 theta steering angle
+%
+%OUT
+%[]              - The figure plot
+%
+%Created by Jørgen Grythe, Norsonic AS
+%Last updated 2015-10-27
+
 
 if ~exist('thetaSteeringAngle','var')
     thetaSteeringAngle = 0;
 end
 
 if ~exist('theta','var')
-    thetaScanningAngle = -90:0.2:90;
+    thetaScanningAngles = -90:0.2:90;
 end
 
 
@@ -19,18 +38,18 @@ lwidth = 1;
 % Plot beampattern
 bpFig = figure;clf
 for ff = f
-	W = arrayFactor(xPos, yPos, w, ff, c, thetaScanningAngle, 0, thetaSteeringAngle);
+	W = arrayFactor(xPos, yPos, w, ff, c, thetaScanningAngles, 0, thetaSteeringAngle);
     W = 20*log10(W);
     
     
     % Rectangular plot
     subplot(211)
-    plot(thetaScanningAngle,W,'DisplayName',[num2str(ff*1e-3) ' kHz'],'linewidth',lwidth);
+    plot(thetaScanningAngles,W,'DisplayName',[num2str(ff*1e-3) ' kHz'],'linewidth',lwidth);
     hold on
     
     % Polar plot
     subplot(212)
-    p = mmpolar(thetaScanningAngle*pi/180,W,...
+    p = mmpolar(thetaScanningAngles*pi/180,W,...
     'Style','Compass',...
     'RLimit',[-dBmin 0],...
     'TLimit',[-pi/2 pi/2],...
@@ -50,7 +69,7 @@ xlabel('Angle (deg)')
 ylabel('Attenuation (dB)')
 
 grid on
-axis([thetaScanningAngle(1) thetaScanningAngle(end) -dBmin 0])
+axis([thetaScanningAngles(1) thetaScanningAngles(end) -dBmin 0])
 legend('show','Location','NorthEast')
 
 set(gca,'YTick',[-50 -45 -40 -35 -30 -25 -20 -15 -10 -6 -3 0])
