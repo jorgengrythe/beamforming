6d18bfa6116968ce4dca2cb1ef5530f70cc8bad3
Jørgen Grythe <jorgen.grythe@gmail.com>
2016-07-01 10:29:18 +0200

    Changed alterations to theta instead of phi

--- algorithm/convertCartesianToPolar.m
+++ algorithm/convertCartesianToPolar.m
@@ -8,7 +8,7 @@ phiAngles = atan(yPos./xPos);
 
 thetaAngles = thetaAngles*180/pi;
 phiAngles = phiAngles*180/pi;
-phiAngles(xPos<0) = phiAngles(xPos<0) + 180;
+thetaAngles(xPos<0) = -thetaAngles(xPos<0);
 
 thetaAngles(isnan(thetaAngles)) = 0;
 phiAngles(isnan(phiAngles)) = 0;
