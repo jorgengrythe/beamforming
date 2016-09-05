0b8af19cdc7275ad0aa547c1a74473d1e7c2398e
Jørgen Grythe <jorgen.grythe@gmail.com>
2016-09-05 12:21:35 +0200

    Added help text and updated some variables

--- algorithm/deconvolutionCleanSC.m
+++ algorithm/deconvolutionCleanSC.m
@@ -1,13 +1,30 @@
 function Q = deconvolutionCleanSC(D, e, w)
+%deconvolutionCleanSC - deconvolves the intensity plot with the clean-sc algorithm
+%as implemented in "CLEAN based on spatial source coherence", Pieter Sijtsma, 2007
+%
+%Q = deconvolutionCleanSC(D, e, w)
+%
+%IN
+%D - PxP cross spectral matrix (CSM)
+%e - NxMxP steering vector/matrix 
+%w - 1xP weighting vector
+%
+%OUT
+%Q - NxM devonvolved intensity plot
+%
+%Created by J?rgen Grythe, Squarehead Technology AS
+%Last updated 2016-09-05
+
 
 [numberOfScanningPointsY, numberOfScanningPointsX, nSensors] = size(e);
-maxIterations = 100;
 
+%Maximum number of iterations to create the clean map
+maxIterations = 100;
 
 %Safety factor 0 < loopGain < 1
 loopGain = 0.9;
 
-%Initialise cross spectral matrix (CSM)
+%Initialise trimmed cross spectral matrix (CSM) by setting the diagonal to zero
 D(logical(eye(nSensors))) = 0;
 
 %Initialise final clean image
@@ -45,15 +62,16 @@ for cleanMapIterations = 1:maxIterations
     % Steering vector to location of peak source
     g = reshape(e(maxPeakValueYIndx, maxPeakValueXIndx, :), nSensors, 1);
     
-    %Get value of source component, initialise h as gmax
+    %Get value of source component, initialise h as steering vector to peak
+    %source
     h = g;
     for iterH = 1:maxIterations
-        hOld = h;
+        hOldValue = h;
         H = h*h';
         
         H(~logical(eye(nSensors))) = 0;
         h = 1/sqrt(1+(w.*g')*H*(g.*w'))*(D*(g.*w')/maxPeakValue + H*(g.*w'));
-        if norm(h-hOld) < 1e-6
+        if norm(h-hOldValue) < 1e-6
             break;
         end
     end
