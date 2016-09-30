9682a409c24a4181fe4848c99ad9f5661e0a1096
Jørgen Grythe <jorgen@sqhead.com>
2016-09-30 10:02:42 +0200

    Meshgrid of evenly spaced points rather than evenly spaced angles

---
+++ plot/meshgridScanPoints.m
@@ -0,0 +1,12 @@
+function [scanPointsX, scanPointsY, thetaScanAngles, phiScanAngles] = meshgridScanPoints(maxX, maxY, zDist, nPointsX, nPointsY)
+
+
+%(x,y) position of scanning points
+scanAxisX = -maxX:2*maxX/(nPointsX-1):maxX;
+scanAxisY = -maxY:2*maxY/(nPointsY-1):maxY;
+
+%Scanning points in meshgrid
+[scanPointsX, scanPointsY] = meshgrid(scanAxisX, scanAxisY);
+
+%Calculate theta, phi angles to each scanning point
+[thetaScanAngles, phiScanAngles] = convertCartesianToPolar(scanPointsX, scanPointsY, zDist);
