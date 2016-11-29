b60a2bb1f1f52bcd525c545a4c171b7901a469d7
Jørgen Grythe <jorgen@sqhead.com>
2016-11-29 10:05:56 +0100

    Renamed and updated description

---
+++ algorithm/convertCartesianToSpherical.m
@@ -0,0 +1,28 @@
+function [thetaAngles, phiAngles] = convertCartesianToSpherical(xPos, yPos, zPos)
+% Get spherical angles in 3D space from cartesian coordinates
+%
+%[thetaAngles, phiAngles] = convertCartesianToSpherical(xPos, yPos, zPos)
+%
+%IN
+%xPos - x-positions [m]
+%yPos - y-positions [m]
+%zPos - z-position [m]
+%
+%OUT
+%thetaAngles - theta angles [deg]
+%phiAngles - phi angles [deg]
+%
+%Created by J?rgen Grythe, Squarehead Technology AS
+%Last updated 2016-11-29
+
+thetaAngles = atan(sqrt(xPos.^2+yPos.^2)./zPos);
+phiAngles = atan(yPos./xPos);
+
+thetaAngles = thetaAngles*180/pi;
+phiAngles = phiAngles*180/pi;
+thetaAngles(xPos<0) = -thetaAngles(xPos<0);
+
+thetaAngles(isnan(thetaAngles)) = 0;
+phiAngles(isnan(phiAngles)) = 0;
+
+end
