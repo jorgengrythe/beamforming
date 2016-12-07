d2aaef38979ac6a0ac6e8e61e9d7d5ef75d45040
Jørgen Grythe <jorgen@sqhead.com>
2016-12-07 18:11:43 +0100

    Updated to full spherical display

--- plot/sphericalCoordinateSystem.m
+++ plot/sphericalCoordinateSystem.m
@@ -1,55 +1,57 @@
 %% Help figure to visualise vectors in spherical coordinates
 
-theta = 26;
-phi = 0;
+%Set angles to vector that shall be visualised
+theta = 45;
+phi = -20;
+
 
 %Vector coordinates
-lx = sin(theta*pi/180)*cos(phi*pi/180);
-ly = sin(theta*pi/180)*sin(phi*pi/180);
-lz = cos(theta*pi/180);
+u = sin(theta*pi/180)*cos(phi*pi/180);
+v = sin(theta*pi/180)*sin(phi*pi/180);
+w = cos(theta*pi/180);
 
 %Half sphere
 [sx, sy, sz] = sphere(100);
-sz(find(sz < 0)) = 0;
+%sz(find(sz < 0)) = 0;
 
 %Set color for lines ++
 cmap = [1 1 1];
 
-h = figure(1); clf
-set(gcf,'color','w')
-hold on
+fig = figure;
+fig.Color = 'w';
+ax = axes('Parent', fig);
+hold(ax, 'on')
 
 %Plot sphere
-surf(sx,sy,sz,'edgecolor','none','Facecolor',cmap,'FaceAlpha',0.2)
-plot(cos(0:pi/50:2*pi),sin(0:pi/50:2*pi),'Color',cmap)
+surf(ax, sx,sy,sz, 'edgecolor', 'none', 'Facecolor', cmap, 'FaceAlpha', 0.2)
+plot(ax, cos(0:pi/50:2*pi), sin(0:pi/50:2*pi), 'Color', cmap)
 
 %Plot the vector with help lines
-line([0 lx],[0 ly],[0 lz],'LineWidth',1.5,'Color',[1.0000 0.9059 0.0941])
-line([lx lx],[ly ly],[0 lz],'Color',cmap,'LineStyle',':')
-line([0 lx],[0 ly],[0 0],'Color',cmap,'LineStyle',':')
-line([0 lx],[0 ly],[lz lz],'Color',cmap,'LineStyle',':')
+line(ax, [0 u], [0 v], [0 w], 'LineWidth', 1.5, 'Color', [1.0000 0.9059 0.0941])
+line(ax, [u u], [v v], [0 w], 'Color', cmap, 'LineStyle',':')
+line(ax, [0 u], [0 v], [0 0], 'Color', cmap, 'LineStyle',':')
+line(ax, [0 u], [0 v], [w w], 'Color', cmap, 'LineStyle',':')
 
 %Plot coordinate system lines
-%x-lines
-line([0 1],[0 0],[0 0],'Color',cmap)
-line([-1 1],[1 1],[0 0],'Color',cmap)
-%y-lines
-line([0 0],[0 1],[0 0],'Color',cmap)
-line([-1 -1],[-1 1],[0 0],'Color',cmap)
-%z-lines
-line([0 0],[0 0],[0 1],'Color',cmap)
-line([-1 -1],[1 1],[0 1],'Color',cmap)
-%text
-text(0.8,-0.1,'x','color',cmap)
-text(-0.2,0.9,'y','color',cmap)
-text(0,-0.1,0.9,'z','color',cmap)
-text(lx/10,ly/10,0.2,'\theta','color',cmap)
-text(0.1,0.2,0,'\phi','color',cmap)
+line(ax, [0 1], [0 0], [0 0], 'Color', cmap)
+line(ax, [0 0], [0 1], [0 0], 'Color', cmap)
+line(ax, [0 0], [0 0], [-1 1], 'Color', cmap)
+text(ax, 0.8, -0.1, 'u', 'color', cmap, 'fontweight', 'bold')
+text(ax, -0.2, 0.9, 'v', 'color', cmap, 'fontweight', 'bold')
+text(ax, 0, -0.1, 0.9, 'w', 'color', cmap, 'fontweight', 'bold')
+text(ax, u/10, v/10,0.2, '\theta', 'color', cmap)
+text(ax, 0.1, 0.2,0, '\phi', 'color', cmap)
 
 %Set colors and view axis ++
-view(30, 30)
-set(gca,'color',[0 0 0],'xcolor',cmap,'ycolor',cmap,'zcolor',cmap)
-set(gca,'XTickLabel',[],'YTickLabel',[],'ZTickLabel',[])
-set(gca,'XMinorGrid','on','YMinorGrid','on','ZMinorGrid','on','MinorGridColor',cmap,'MinorGridLineStyle','-')
-axis equal
-title(['\theta = ' num2str(theta) ', \phi = ' num2str(phi)],'FontWeight','Normal')
+view(ax, 30, 15)
+ax.Color = 'k';
+ax.XColor = cmap;
+ax.YColor = cmap;
+ax.ZColor = cmap;
+ax.XMinorGrid = 'on';
+ax.YMinorGrid = 'on';
+ax.ZMinorGrid = 'on';
+ax.MinorGridColor = cmap;
+ax.MinorGridLineStyle = '-';
+axis(ax, 'equal')
+title(ax, ['\theta = ' num2str(theta) ', \phi = ' num2str(phi)], 'FontWeight','Normal')
